3e1dbf48fe6bbf7e4222aae5448db118a55389d8
Marcin Bajek <bajekmarcin@gmail.com>
2020-02-17 21:42:53 +0100

    Change Login view

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import styles from './Login.module.scss';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 
 const Login = () => (
   <div className={styles.component}>
     <div>
-      <span className={styles.login}>Login</span>
-      <input type='text'></input>
+      <TextField
+        id="login"
+        label="Login"
+        type="text"
+        defaultValue= {''}
+      />
     </div>
     <div>
-      <span className={styles.password}>Password</span>
-      <input type='password'></input>
+      <TextField
+        id="password"
+        label="Password"
+        type="password"
+        defaultValue= {''}
+      />
     </div>
     <Button>Login</Button>
   </div>
